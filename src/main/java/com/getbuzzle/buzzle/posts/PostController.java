80464d1a0eb287500cfd85c809ed57f5060bf991
Muhammad Irtiza <muhammad.irtiza751@gmail.com>
2025-04-02 15:32:32 +0500

    Added the add new post controller and returning the proper response status.

--- src/main/java/com/getbuzzle/buzzle/posts/PostController.java
+++ src/main/java/com/getbuzzle/buzzle/posts/PostController.java
@@ -1,8 +1,8 @@
 package com.getbuzzle.buzzle.posts;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,9 +10,9 @@ import java.util.stream.Collectors;
 
 @RestController("/posts")
 public class PostController {
-    public List<Post> posts = new ArrayList<>();
+    private List<Post> posts = new ArrayList<>();
 
-    public PostController() {
+    private PostController() {
         this.posts.add(new Post(1, "Content 1", "https://www.imageurl.com"));
         this.posts.add(new Post(2, "Content 2", "https://www.imageurl.com"));
         this.posts.add(new Post(3, "Content 3", "https://www.imageurl.com"));
@@ -21,9 +21,19 @@ public class PostController {
     }
 
     @GetMapping
-    List<Post> getPosts(
+    public ResponseEntity<List<Post>> getPosts(
             @RequestParam(defaultValue = "10") int limit
     ) {
-        return this.posts.stream().limit(Math.min(limit, posts.size())).collect(Collectors.toList());
+        List<Post> response = this.posts
+                .stream()
+                .limit(Math.min(limit, posts.size()))
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Post> addPost(@RequestBody Post postData) {
+        this.posts.add(postData);
+        return new ResponseEntity<>(postData, HttpStatus.CREATED);
     }
 }
